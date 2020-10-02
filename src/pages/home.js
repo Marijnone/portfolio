79f55878e6e2526de5e9bf69cb5035508c415427
Marijn <marijn.moviat@hva.nl>
2020-10-02 12:00:56 +0200

    Added scss recovered x y animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,32 +8,31 @@ import Work from '../components/Work'
 import Worknew from '../components/Work-new'
 
 //Hooks
-import UseMousePosition from '../hooks/getMousePosition'
 
 // const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }
 
 const Home = () => {
-  const { x, y } = UseMousePosition()
-  const animation = useAnimation()
-  const [contentRef, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: '-300px',
-  })
+  // const { x, y } = UseMousePosition()
+  // const animation = useAnimation()
+  // const [contentRef, inView] = useInView({
+  //   triggerOnce: true,
+  //   rootMargin: '-300px',
+  // })
 
   //Prevent build error
   const isSSR = typeof window === 'undefined'
 
-  useEffect(() => {
-    if (inView) {
-      animation.start('visible')
-    }
-  }, [animation, inView])
+  // useEffect(() => {
+  //   if (inView) {
+  //     animation.start('visible')
+  //   }
+  // }, [animation, inView])
   return (
     <>
       {!isSSR && <Shapes />}
       {!isSSR && <Intro />}
-      {!isSSR && <Work />}
-      {/* <Worknew /> */}
+      {/* {!isSSR && <Work />} */}
+      <Worknew />
       {/* <Music /> */}
     </>
   )
